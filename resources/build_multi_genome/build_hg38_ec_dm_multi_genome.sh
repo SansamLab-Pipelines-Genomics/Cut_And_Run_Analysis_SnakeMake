77fdd12634f99332576454460a92f3e2e03ecd12
sansamcl <63733772+sansamcl@users.noreply.github.com>
2022-05-09 16:32:31 -0500

    Update build_hg38_ec_dm_multi_genome.sh

--- resources/build_multi_genome/build_hg38_ec_dm_multi_genome.sh
+++ resources/build_multi_genome/build_hg38_ec_dm_multi_genome.sh
@@ -1,1 +1,22 @@
 #!/usr/bin/bash -l
+
+# get hg38
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz
+# modify chromosome names
+
+
+# get drosophila genome
+wget https://ftp.ncbi.nlm.nih.gov/genomes/refseq/invertebrate/Drosophila_melanogaster/latest_assembly_versions/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna.gz
+# modify chromosome names
+zcat GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna.gz | sed 's/>/>DM_/g' > dm.fasta
+
+# get e coli genome
+wget https://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/Escherichia_coli/reference/GCF_000008865.2_ASM886v2/GCF_000008865.2_ASM886v2_genomic.fna.gz
+# modify chromosome names
+zcat GCF_000008865.2_ASM886v2_genomic.fna.gz | sed 's/>/>EC_/g' > ec.fasta
+
+# concatenate fasta files
+cat dm.fasta ec.fasta | gzip > dm_ec.fna.gz
+zcat GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz dm_ec.fna.gz | gzip > GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set_dm_ec.fna.gz
+
+# index combined genome with bowtie2
