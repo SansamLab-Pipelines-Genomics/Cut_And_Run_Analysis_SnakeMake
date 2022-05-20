321a5e4f6da454a39554251c84f551fa5fbf4550
sansamcl <63733772+sansamcl@users.noreply.github.com>
2022-05-20 13:59:09 -0500

    Update make_md5_hashes.sh

--- resources/useful_scripts/make_md5_hashes.sh
+++ resources/useful_scripts/make_md5_hashes.sh
@@ -5,12 +5,12 @@
 ## make directory for the md5sum hash files
 mkdir -p md5_hashes
 
-md5sum results/aligned/*.bam > md5_hashes/aligned.md5
-md5sum results/aligned/*.bai >> md5_hashes/aligned.md5
-md5sum results/aligned_speciesOfInterest/*.bam > md5_hashes/aligned_speciesOfInterest.md5
-md5sum results/aligned_speciesOfInterest/*.bai >> md5_hashes/aligned_speciesOfInterest.md5
-md5sum results/bigwigs_no_spikein/*.bw > md5_hashes/bigwigs_no_spikein.md5
-md5sum results/bigwigs_spikein/*.bw > md5_hashes/bigwigs_spikein.md5
-md5sum results/sicer/* > md5_hashes/sicer.md5
-md5sum results/macs2_normalPeaks/* > md5_hashes/macs2_normalPeaks.md5
-md5sum results/macs2_broadPeaks/* > md5_hashes/macs2_broadPeaks.md5
+md5sum results/aligned/*.bam > results/md5_hashes/aligned.md5
+md5sum results/aligned/*.bai >> results/md5_hashes/aligned.md5
+md5sum results/aligned_speciesOfInterest/*.bam > results/md5_hashes/aligned_speciesOfInterest.md5
+md5sum results/aligned_speciesOfInterest/*.bai >> results/md5_hashes/aligned_speciesOfInterest.md5
+md5sum results/bigwigs_no_spikein/*.bw > results/md5_hashes/bigwigs_no_spikein.md5
+md5sum results/bigwigs_spikein/*.bw > results/md5_hashes/bigwigs_spikein.md5
+md5sum results/sicer/* > results/md5_hashes/sicer.md5
+md5sum results/macs2_normalPeaks/* > results/md5_hashes/macs2_normalPeaks.md5
+md5sum results/macs2_broadPeaks/* > results/md5_hashes/macs2_broadPeaks.md5
