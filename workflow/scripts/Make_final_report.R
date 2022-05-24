45e362847dbe4cc78ed71fed6e1fa55040ebc7e1
sansamcl <63733772+sansamcl@users.noreply.github.com>
2022-05-24 10:13:13 -0500

    Update Make_final_report.R

--- workflow/scripts/Make_final_report.R
+++ workflow/scripts/Make_final_report.R
@@ -12,9 +12,4 @@ library(magick)
 
 # get arguments from Snakemake S4 object
 samples <- snakemake@params[["samples"]]
-class(samples)
-dim(samples)
-length(samples)
-samples
-#samples <- strsplit(snakemake@params[["samples"]], split = ",")[[1]]
-#rmarkdown::render("resources/Report.Rmd",output_dir="results/",params=list(samples=samples))
+rmarkdown::render("resources/Report.Rmd",output_dir="results/",params=list(samples=samples))
